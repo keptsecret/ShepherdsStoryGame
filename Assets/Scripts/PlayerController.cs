95af92fcb67c5f6c9ef07d6a37d535ab19303a8c
keptsecret <sorchon@gmail.com>
2023-10-21 15:05:04 -0400

    fix younggi merge conflicts and adjust player camera interaction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,19 +6,35 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    private Animator anim;
     private Vector2 movement;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
         Vector3 dir = new Vector3(movement.x, 0f, movement.y);
-        Vector3 dirCamera = WorldToCameraSpace(dir);
-        rb.MovePosition(rb.transform.position + dirCamera.normalized * 0.25f);
-        rb.transform.rotation = (Camera.main.transform.rotation);
+        Vector3 cameraDir = WorldToCameraSpace(dir);
+        rb.MovePosition(rb.transform.position + cameraDir.normalized * 0.1f);
+
+        Vector3 lookDir = WorldToCameraSpace(Vector3.forward);
+        Quaternion lookRotation = Quaternion.LookRotation(lookDir.normalized, rb.transform.up);
+        float diffRotation = lookRotation.eulerAngles.y - rb.transform.eulerAngles.y;
+        float eulerY = rb.transform.eulerAngles.y;
+
+        if (diffRotation > 0f || diffRotation < 0f)
+        {
+            eulerY = lookRotation.eulerAngles.y;
+        }
+        Vector3 eulerRotation = new Vector3(0, eulerY, 0);
+        rb.MoveRotation(Quaternion.Euler(eulerRotation));
+
+        anim.SetFloat("velx", movement.x);
+        anim.SetFloat("vely", movement.y);
     }
 
     void OnMove(InputValue value)
